5ba6eb4dfb02f53ad4c5d80a371122f5476df428
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-10-28 17:12:40 +0100

    Update Avatar.tsx

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,5 +1,4 @@
 import * as ReactAvatar from "@radix-ui/react-avatar";
-import clsx from "clsx";
 import React, { PropsWithChildren } from "react";
 import { cls } from "tagged-classnames-free";
 export type AvartarProps = PropsWithChildren<
@@ -9,19 +8,18 @@ export type AvartarProps = PropsWithChildren<
 export function Avatar({ alt, src, children }: AvartarProps) {
   return (
     <ReactAvatar.Root
-      className={cls`inline-flex h-[45px]
-      w-[45px] select-none items-center justify-center overflow-hidden rounded-[100%]
-      bg-[color:var(--black-a3)] align-middle 
-      `}
+      className={cls`inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden 
+      rounded-[100%] bg-[color:var(--black-a3)] align-middle`}
     >
       <ReactAvatar.Image
         sizes=""
-        className="h-full w-full rounded-[inherit] object-cover"
+        className={cls`h-full w-full rounded-[inherit] object-cover`}
         src={src}
         alt={alt}
       />
       <ReactAvatar.Fallback
-        className="flex h-full w-full items-center justify-center bg-[white] text-[15px] font-medium leading-none text-[color:var(--violet-11)]"
+        className={cls`flex h-full w-full items-center justify-center bg-[white] text-[15px] font-medium 
+        leading-none text-[color:var(--violet-11)]`}
         delayMs={600}
       >
         {children}
