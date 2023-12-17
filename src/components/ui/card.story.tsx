e1b8c9ae3fe0137dfc3e72f8455916c6a3bddcb4
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 21:50:01 +0000

    .

--- src/components/ui/card.story.tsx
+++ src/components/ui/card.story.tsx
@@ -7,6 +7,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Meta, StoryFn } from "@storybook/react";
+import { Badge } from "./badge";
 
 export default {
   title: "Components/Card",
@@ -24,7 +25,15 @@ const Template: StoryFn = (args) => (
   <Card {...args}>
     <CardHeader>
       <CardTitle>Title of the Card</CardTitle>
-      <CardDescription>Description of what's inside the card.</CardDescription>
+      <CardDescription>
+        <div className="flex w-full flex-col">
+          <p>Description of what's inside the card.</p>
+          <div className="flex gap-1">
+            <Badge variant="default">Hello</Badge>
+            <Badge variant="secondary">Hello</Badge>
+          </div>
+        </div>
+      </CardDescription>
     </CardHeader>
     <CardContent>Content of the card goes here.</CardContent>
     <CardFooter>Footer content here.</CardFooter>
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,77 +1,68 @@
-import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
+import { tv } from "tailwind-variants";
+
+const card = tv({
+  slots: {
+    base: "rounded-lg border bg-card text-card-foreground shadow-sm",
+    header: "flex flex-col space-y-1.5 p-6",
+    title: "text-2xl font-semibold leading-none tracking-tight",
+    description: "text-sm text-muted-foreground",
+    content: "p-6 pt-0",
+    footer: "flex items-center p-6 pt-0",
+  },
+});
 
 const Card = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      className={cn(
-        "rounded-lg border bg-card text-card-foreground shadow-sm",
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  ),
+  ({ className, ...props }, ref) => {
+    const { base } = card();
+    return <div className={base({ className })} ref={ref} {...props} />;
+  },
 );
 Card.displayName = "Card";
 
 const CardHeader = forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
-    ref={ref}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const { header } = card();
+  return <div className={header({ className })} ref={ref} {...props} />;
+});
 CardHeader.displayName = "CardHeader";
 
 const CardTitle = forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
-  <h3
-    className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
-      className,
-    )}
-    ref={ref}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const { title } = card();
+  return <h3 className={title({ className })} ref={ref} {...props} />;
+});
 CardTitle.displayName = "CardTitle";
 
 const CardDescription = forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <p
-    className={cn("text-sm text-muted-foreground", className)}
-    ref={ref}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const { description } = card();
+  return <p className={description({ className })} ref={ref} {...props} />;
+});
 CardDescription.displayName = "CardDescription";
 
 const CardContent = forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div className={cn("p-6 pt-0", className)} ref={ref} {...props} />
-));
+>(({ className, ...props }, ref) => {
+  const { content } = card();
+  return <div className={content({ className })} ref={ref} {...props} />;
+});
 CardContent.displayName = "CardContent";
 
 const CardFooter = forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    className={cn("flex items-center p-6 pt-0", className)}
-    ref={ref}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const { footer } = card();
+  return <div className={footer({ className })} ref={ref} {...props} />;
+});
 CardFooter.displayName = "CardFooter";
 
 export {
