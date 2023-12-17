10395df1e186deed412d70b565b7046b81bde0ba
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 18:50:40 +0000

    .

---
+++ src/components/Tabs.story.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/components/Button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/Card";
+import { Input } from "@/components/Input";
+import { Label } from "@/components/Label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/Tabs";
+import { Meta } from "@storybook/react";
+
+export default {
+  component: Tabs,
+  subcomponents: { TabsList, TabsTrigger, TabsContent },
+} as Meta;
+
+export function TabsDemo() {
+  return (
+    <Tabs className="w-[400px]" defaultValue="account">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input defaultValue="Pedro Duarte" id="name" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input defaultValue="@peduarte" id="username" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
+}
