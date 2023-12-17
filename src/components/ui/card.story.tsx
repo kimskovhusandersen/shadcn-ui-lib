60fcec09538195a72cc5b2a628a6f949618ca31a
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 22:41:52 +0000

    Update card.story.tsx

--- src/components/ui/card.story.tsx
+++ src/components/ui/card.story.tsx
@@ -39,37 +39,31 @@ export function CardWithForm() {
 
 export function ProductCard() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <Card className="group relative w-[350px]  hover:drop-shadow-lg">
-          <CardHeader className="p-2">
-            <div className="aspect-h-1 aspect-w-1 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-              <img
-                alt="Front of men&#039;s Basic Tee in black."
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-              />
-            </div>
-          </CardHeader>
-          <div className="flex justify-between p-6">
-            <div>
-              <CardTitle>
-                <a
-                  className="after:absolute after:bottom-0 after:left-0 after:right-0 after:top-0 after:z-10"
-                  href="#"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  Basic Tee
-                </a>
-              </CardTitle>
-              <p className="mt-1 text-sm text-gray-500">Aspen White</p>
-            </div>
-            <p className="text-xl font-semibold leading-none tracking-tight">
-              $35
-            </p>
-          </div>
-        </Card>
+    <Card className="group relative w-[350px]  hover:drop-shadow-lg">
+      <CardHeader className="p-2">
+        <div className="aspect-h-1 aspect-w-1 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+          <img
+            alt="Front of men&#039;s Basic Tee in black."
+            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+          />
+        </div>
+      </CardHeader>
+      <div className="flex justify-between p-6">
+        <div>
+          <CardTitle>
+            <a
+              className="after:absolute after:bottom-0 after:left-0 after:right-0 after:top-0 after:z-10"
+              href="#"
+              onClick={(e) => e.preventDefault()}
+            >
+              Basic Tee
+            </a>
+          </CardTitle>
+          <p className="mt-1 text-sm text-gray-500">Aspen White</p>
+        </div>
+        <p className="text-xl font-semibold leading-none tracking-tight">$35</p>
       </div>
-    </div>
+    </Card>
   );
 }
