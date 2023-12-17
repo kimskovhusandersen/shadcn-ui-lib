f79ea94da1c3c846cc1089439b2a7b1ab0eb3b31
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 19:23:10 +0000

    Create Separator.story.tsx

---
+++ src/components/Separator.story.tsx
@@ -0,0 +1,22 @@
+import { Meta } from "@storybook/react";
+import { Separator } from "./Separator";
+
+export default {
+  component: Separator,
+} as Meta;
+
+export const Horizontal = () => (
+  <div className="flex space-x-4">
+    <div className="h-24 w-24 rounded-md bg-gray-200" />
+    <Separator orientation="horizontal" />
+    <div className="h-24 w-24 rounded-md bg-gray-200" />
+  </div>
+);
+
+export const Vertical = () => (
+  <div className="flex space-x-4">
+    <div className="h-24 w-24 rounded-md bg-gray-200" />
+    <Separator orientation="vertical" />
+    <div className="h-24 w-24 rounded-md bg-gray-200" />
+  </div>
+);
