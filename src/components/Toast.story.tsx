9e53f488fde7066ab6f84f1170e528a839978083
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 19:01:27 +0000

    Revert "Delete Toast.story.tsx"
    This reverts commit 9a729f3038cbd1d5e297ce9d979aa7a74fa02eac.

---
+++ src/components/Toast.story.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/Button";
+import { Toast, ToastAction, ToastProvider } from "@/components/Toast";
+import { useToast } from "@/hooks/use-toast";
+import { Meta } from "@storybook/react";
+
+export default {
+  component: ToastAction,
+} as Meta;
+
+export function ToastDemo() {
+  const { toast } = useToast();
+
+  return (
+    <>
+      <ToastProvider>
+        <Button
+          onClick={() => {
+            toast({
+              title: "Scheduled: Catch up ",
+              description: "Friday, February 10, 2023 at 5:57 PM",
+              action: (
+                <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+              ),
+            });
+          }}
+          variant="outline"
+        >
+          Add to calendar
+        </Button>
+        <Toast>sdfdf</Toast>
+      </ToastProvider>
+    </>
+  );
+}
