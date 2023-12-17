0fafb1b8d9d10dbf3f48a85dd5f7ed5bbe1601f3
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 18:48:20 +0000

    .

---
+++ src/components/Sheet.story.tsx
@@ -0,0 +1,72 @@
+import { Meta } from "@storybook/react";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import { Label } from "./Label";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetHeader,
+  SheetContent,
+  SheetTitle,
+  SheetDescription,
+  SheetFooter,
+  SheetClose,
+} from "./Sheet";
+
+export default {
+  component: Sheet,
+  subcomponents: {
+    SheetTrigger,
+    SheetHeader,
+    SheetContent,
+    SheetTitle,
+    SheetDescription,
+    SheetFooter,
+    SheetClose,
+  },
+} as Meta;
+
+const SHEET_SIDES = ["top", "right", "bottom", "left"] as const;
+
+type SheetSide = (typeof SHEET_SIDES)[number];
+
+export function SheetSide() {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      {SHEET_SIDES.map((side) => (
+        <Sheet key={side}>
+          <SheetTrigger asChild>
+            <Button variant="outline">{side}</Button>
+          </SheetTrigger>
+          <SheetContent side={side}>
+            <SheetHeader>
+              <SheetTitle>Edit profile</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here. Click save when you're done.
+              </SheetDescription>
+            </SheetHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label className="text-right" htmlFor="name">
+                  Name
+                </Label>
+                <Input className="col-span-3" id="name" value="Pedro Duarte" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label className="text-right" htmlFor="username">
+                  Username
+                </Label>
+                <Input className="col-span-3" id="username" value="@peduarte" />
+              </div>
+            </div>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button type="submit">Save changes</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      ))}
+    </div>
+  );
+}
