a3d0d50c44a4490f9abdd318405bb86278f2cfc1
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 15:45:06 +0000

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,20 @@
+import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
-import { tv } from "tailwind-variants";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-const input = tv({
-  base: "border-input bg-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
-});
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
-      <input className={input(className)} ref={ref} type={type} {...props} />
+      <input
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        type={type}
+        {...props}
+      />
     );
   },
 );
