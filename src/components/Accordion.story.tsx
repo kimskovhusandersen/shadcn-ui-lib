f52aede3af7f448fc195e4b80313a05c3e080bd6
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 16:14:20 +0000

    .

---
+++ src/components/Accordion.story.tsx
@@ -0,0 +1,36 @@
+import { StoryFn, Meta } from "@storybook/react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "./Accordion";
+
+export default {
+  title: "Components/Accordion",
+  component: Accordion,
+  subcomponents: { AccordionItem, AccordionTrigger, AccordionContent },
+} as Meta;
+
+const Template: StoryFn<typeof Accordion> = (args) => (
+  <Accordion {...args}>
+    <AccordionItem value="item-1">
+      <AccordionTrigger>Item 1</AccordionTrigger>
+      <AccordionContent>Content for Item 1</AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-2">
+      <AccordionTrigger>Item 2</AccordionTrigger>
+      <AccordionContent>Content for Item 2</AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-3">
+      <AccordionTrigger>Item 3</AccordionTrigger>
+      <AccordionContent>Content for Item 3</AccordionContent>
+    </AccordionItem>
+  </Accordion>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  type: "single",
+  // Args for default Accordion
+};
