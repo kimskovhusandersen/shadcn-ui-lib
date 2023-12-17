708f10c8396b0c0c3ee699c8d9a64a68646b13d9
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 19:22:06 +0000

    .

---
+++ src/components/ScrollArea.story.tsx
@@ -0,0 +1,25 @@
+import { ScrollArea } from "@/components/ScrollArea";
+import { Separator } from "@/components/Separator";
+
+export default {};
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`,
+);
+
+export function ScrollAreaDemo() {
+  return (
+    <ScrollArea className="h-72 w-48 rounded-md border">
+      <div className="p-4">
+        <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+        {tags.map((tag) => (
+          <>
+            <div className="text-sm" key={tag}>
+              {tag}
+            </div>
+            <Separator className="my-2" />
+          </>
+        ))}
+      </div>
+    </ScrollArea>
+  );
+}
