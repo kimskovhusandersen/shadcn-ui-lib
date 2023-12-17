d2a6822e790d76b9c183d072084d2d9e05fb65fd
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-17 18:09:09 +0000

    Update Form.story.tsx

--- src/components/Form.story.tsx
+++ src/components/Form.story.tsx
@@ -26,9 +26,7 @@ export default {
 };
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  username: z.string({ required_error: "Please enter a username" }),
 });
 
 export function ProfileForm() {
