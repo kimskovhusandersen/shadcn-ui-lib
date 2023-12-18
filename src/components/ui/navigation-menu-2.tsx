99f5ed903907dd4ff01a25149fa70d731b86952b
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-18 20:24:53 +0000

    .

--- src/components/ui/navigation-menu-2.tsx
+++ src/components/ui/navigation-menu-2.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { forwardRef } from "react";
+import { Cart } from "./cart";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -526,26 +527,7 @@ const DesktopNavigationMenu = forwardRef<
 
             {/* <!-- Cart --> */}
             <div className="ml-4 flow-root lg:ml-6">
-              <a className="group -m-2 flex items-center p-2" href="#">
-                <svg
-                  aria-hidden="true"
-                  className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="1.5"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                  0
-                </span>
-                <span className="sr-only">items in cart, view bag</span>
-              </a>
+              <Cart />
             </div>
           </div>
         </div>
