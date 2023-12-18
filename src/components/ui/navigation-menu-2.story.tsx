a7d29ccf1d205f2304e78275eb47350b0f5c57a7
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-18 19:41:18 +0000

    Update navigation-menu-2.story.tsx

--- src/components/ui/navigation-menu-2.story.tsx
+++ src/components/ui/navigation-menu-2.story.tsx
@@ -3,7 +3,7 @@ import { DesktopNavigationMenu, NavigationMenu } from "./navigation-menu-2";
 
 export default {
   component: NavigationMenu,
-  title: "NavigationMenu2",
+  title: "Components/NavigationMenu2",
 } as Meta;
 
 export const Default = () => (
