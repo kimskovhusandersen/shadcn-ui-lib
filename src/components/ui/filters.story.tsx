5aa908c4a99c891da4f27c32cc402fa6eb753ebb
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-18 20:51:02 +0000

    Create filters.story.tsx

---
+++ src/components/ui/filters.story.tsx
@@ -0,0 +1,9 @@
+import { Meta } from "@storybook/react";
+import { Filters } from "./filters";
+
+export default {
+  title: "Components/Filters",
+  component: Filters,
+} as Meta;
+
+export const Primary = () => <Filters />;
