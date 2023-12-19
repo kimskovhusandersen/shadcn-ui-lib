48aef3d917492684c87a46e5d944fe5999178db1
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-19 13:39:23 +0000

    .

---
+++ src/forms/login-form.story.tsx
@@ -0,0 +1,9 @@
+import { Meta } from "@storybook/react";
+import { LoginForm } from "./login-form";
+
+export default {
+  title: "Forms/Login Form",
+  component: LoginForm,
+} as Meta;
+
+export const Default = () => <LoginForm />;
