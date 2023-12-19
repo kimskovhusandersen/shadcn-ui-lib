f5505a7439b2d930f8ca9e20d3af7eb65eb79d1a
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-19 00:23:39 +0000

    .

--- src/pages/product.story.tsx
+++ src/pages/product.story.tsx
@@ -1,9 +1,16 @@
 import { Meta } from "@storybook/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { Product } from "./product";
 
 export default {
   component: Product,
-  title: "Pages/Product",
+  title: "Product",
 } as Meta;
 
-export const Default = () => <Product />;
+export const Default = () => (
+  <MemoryRouter initialEntries={["/men/t-shirts"]}>
+    <Routes>
+      <Route element={<Product />} path="/:categorySlug/:productSlug" />
+    </Routes>
+  </MemoryRouter>
+);
