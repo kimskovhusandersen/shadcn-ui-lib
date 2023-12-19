33cc3ffa94624a461d0d9a462b9c4beba31c87e5
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-19 14:36:51 +0000

    .

---
+++ src/components/ui/navigation-menu-3.story.tsx
@@ -0,0 +1,9 @@
+import { Meta } from "@storybook/react";
+import { NavigationMenuDemo } from "./navigation-menu-3";
+
+export default {
+  component: NavigationMenuDemo,
+  title: "Components/NavigationMenu3",
+} as Meta;
+
+export const Default = () => <NavigationMenuDemo />;
