abebd071f743cfd4031c3821afa3f24d862b083d
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-19 10:51:19 +0000

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Collections } from "@/components/ui/collections";
 import { Footer } from "@/components/ui/footer";
 import {
@@ -12,6 +13,59 @@ export function Home() {
       <NavigationMenu>
         <DesktopNavigationMenu />
       </NavigationMenu>
+
+      <AspectRatio className="border" ratio={16 / 9}>
+        <img
+          alt=""
+          className="h-full w-full object-cover"
+          src="https://dummyimage.com/1200x900/fff/aaa"
+        />
+      </AspectRatio>
+      <div className="sm:grid-cols- grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6">
+        <AspectRatio className="border" ratio={1 / 1}>
+          <img
+            alt=""
+            className="h-full w-full object-cover"
+            src="https://dummyimage.com/200x200/fff/aaa"
+          />
+        </AspectRatio>
+        <AspectRatio className="border" ratio={1 / 1}>
+          <img
+            alt=""
+            className="h-full w-full object-cover"
+            src="https://dummyimage.com/200x200/fff/aaa"
+          />
+        </AspectRatio>
+        <AspectRatio className="border" ratio={1 / 1}>
+          <img
+            alt=""
+            className="h-full w-full object-cover"
+            src="https://dummyimage.com/200x200/fff/aaa"
+          />
+        </AspectRatio>
+        <AspectRatio className="border" ratio={1 / 1}>
+          <img
+            alt=""
+            className="h-full w-full object-cover"
+            src="https://dummyimage.com/200x200/fff/aaa"
+          />
+        </AspectRatio>
+        <AspectRatio className="border" ratio={1 / 1}>
+          <img
+            alt=""
+            className="h-full w-full object-cover"
+            src="https://dummyimage.com/200x200/fff/aaa"
+          />
+        </AspectRatio>
+        <AspectRatio className="border" ratio={1 / 1}>
+          <img
+            alt=""
+            className="h-full w-full object-cover"
+            src="https://dummyimage.com/200x200/fff/aaa"
+          />
+        </AspectRatio>
+      </div>
+
       <Collections />
       <Newsletter />
       <Footer />
