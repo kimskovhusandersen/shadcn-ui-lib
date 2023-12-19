14e0d12344fa11695d30289ac2515fc04f47cbef
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-19 12:42:11 +0000

    .

---
+++ src/components/ui/account-form.story.tsx
@@ -0,0 +1,9 @@
+import { Meta } from "@storybook/react";
+import { AccountForm } from "./account-form";
+
+export default {
+  title: "Components/AccountForm",
+  component: AccountForm,
+} as Meta;
+
+export const Default = () => <AccountForm />;
