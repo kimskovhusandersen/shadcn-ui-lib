33784f2d83558f36c27c1bfb0f6b80dafb566ac1
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-19 00:54:16 +0000

    Update footer.tsx

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,40 +1,45 @@
 export const Footer = () => {
   return (
     <footer className="bg-gray-800 p-24 text-white">
-      <div className="container mx-auto grid grid-cols-1 gap-4 md:grid-cols-4">
+      <div className="container mx-auto grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {/* <!-- Company Info --> */}
         <div>
-          <h2 className="mb-2 text-lg font-bold">Company</h2>
-          <p className="text-gray-400">
-            123 E-commerce St.
-            <br />
-            Shopping City, SC 12345
-          </p>
-          <p className="mt-4 text-gray-400">Email: contact@ecommerce.com</p>
-          <p className="text-gray-400">Phone: (123) 456-7890</p>
+          <h3 className="text-md font-bold">Company</h3>
+          <dl className="mt-4 space-y-2 text-sm">
+            <dt className="sr-only">Street</dt>
+            <dd className="break-words text-gray-400">123 E-commerce St.</dd>
+            <dt className="sr-only">City</dt>
+            <dd className="break-words text-gray-400">
+              Shopping City, SC 12345
+            </dd>
+            <dt className="sr-only">Email</dt>
+            <dd className="break-words text-gray-400">contact@ecommerce.com</dd>
+            <dt className="sr-only">Fax number</dt>
+            <dd className="break-words text-gray-400">(123) 456-7890</dd>
+          </dl>
         </div>
 
         {/* <!-- Customer Service --> */}
         <div>
-          <h2 className="mb-2 text-lg font-bold">Customer Service</h2>
-          <ul>
+          <h3 className="text-md font-bold">Customer Service</h3>
+          <ul className="mt-4 space-y-2">
             <li>
-              <a className="text-gray-400 hover:text-white" href="#">
+              <a className="text-sm text-gray-400 hover:text-white" href="#">
                 Contact Us
               </a>
             </li>
             <li>
-              <a className="text-gray-400 hover:text-white" href="#">
+              <a className="text-sm text-gray-400 hover:text-white" href="#">
                 Returns
               </a>
             </li>
             <li>
-              <a className="text-gray-400 hover:text-white" href="#">
+              <a className="text-sm text-gray-400 hover:text-white" href="#">
                 Site Map
               </a>
             </li>
             <li>
-              <a className="text-gray-400 hover:text-white" href="#">
+              <a className="text-sm text-gray-400 hover:text-white" href="#">
                 FAQ
               </a>
             </li>
@@ -43,15 +48,15 @@ export const Footer = () => {
 
         {/* <!-- Social Media --> */}
         <div>
-          <h2 className="mb-2 text-lg font-bold">Follow Us</h2>
+          <h3 className="text-md font-bold">Follow Us</h3>
           <div className="flex space-x-2">
-            <a className="text-gray-400 hover:text-white" href="#">
+            <a className="text-sm text-gray-400 hover:text-white" href="#">
               Facebook
             </a>
-            <a className="text-gray-400 hover:text-white" href="#">
+            <a className="text-sm text-gray-400 hover:text-white" href="#">
               Twitter
             </a>
-            <a className="text-gray-400 hover:text-white" href="#">
+            <a className="text-sm text-gray-400 hover:text-white" href="#">
               Instagram
             </a>
           </div>
