031e9f7ad7c7aec2d2637ae20772d54238504365
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-19 13:45:43 +0000

    Update login-form.tsx

--- src/forms/login-form.tsx
+++ src/forms/login-form.tsx
@@ -1,21 +1,10 @@
+import { Button, Input, buttonVariants } from "..";
+
 export function LoginForm() {
   return (
     <>
-      {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
@@ -31,9 +20,8 @@ export function LoginForm() {
                 Email address
               </label>
               <div className="mt-2">
-                <input
+                <Input
                   autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   id="email"
                   name="email"
                   required
@@ -51,18 +39,14 @@ export function LoginForm() {
                   Password
                 </label>
                 <div className="text-sm">
-                  <a
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    href="#"
-                  >
+                  <a className={buttonVariants({ variant: "link" })} href="#">
                     Forgot password?
                   </a>
                 </div>
               </div>
               <div className="mt-2">
-                <input
+                <Input
                   autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   id="password"
                   name="password"
                   required
@@ -72,22 +56,14 @@ export function LoginForm() {
             </div>
 
             <div>
-              <button
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                type="submit"
-              >
-                Sign in
-              </button>
+              <Button type="submit">Sign in</Button>
             </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <a
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              href="#"
-            >
-              Start a 14 day free trial
+            Already has an account?{" "}
+            <a className={buttonVariants({ variant: "link" })} href="#">
+              Go to Login
             </a>
           </p>
         </div>
