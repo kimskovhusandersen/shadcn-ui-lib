37b8967a89a63d597a72b1ef0cf7b942e2b02242
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-12-19 00:05:00 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,12 @@
+import { Footer } from "@/components/ui/footer";
+import { Newsletter } from "@/components/ui/newsletter";
+
 export function Home() {
-  return <>Home page</>;
+  return (
+    <>
+      Home page
+      <Newsletter />
+      <Footer />
+    </>
+  );
 }
